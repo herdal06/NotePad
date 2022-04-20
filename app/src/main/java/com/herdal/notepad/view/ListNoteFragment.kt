09ebd2062d65d65389147fc1b6815c790aaac0fd
herdal06 <h.erdal721@gmail.com>
2022-04-20 22:16:56 +0300

    Create list and add note fragments. Add them to navigation

---
+++ app/src/main/java/com/herdal/notepad/view/ListNoteFragment.kt
@@ -0,0 +1,18 @@
+package com.herdal.notepad.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.herdal.notepad.R
+
+
+class ListNoteFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_list_note, container, false)
+
+        return view
+    }
+}
