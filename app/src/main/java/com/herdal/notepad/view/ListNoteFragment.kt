c815d3b8eab79bf74ab85a54ec40e2aa5b4b7830
herdal06 <h.erdal721@gmail.com>
2022-04-21 02:50:31 +0300

    Set click listener on Floating action bar

--- app/src/main/java/com/herdal/notepad/view/ListNoteFragment.kt
+++ app/src/main/java/com/herdal/notepad/view/ListNoteFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.herdal.notepad.R
+import com.herdal.notepad.databinding.FragmentListNoteBinding
+import kotlinx.android.synthetic.main.fragment_list_note.view.*
 
 
 class ListNoteFragment : Fragment() {
 
+    private lateinit var binding: FragmentListNoteBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_list_note, container, false)
 
-        view.flo
+        view.floatingActionButton.setOnClickListener { view ->
+            findNavController().navigate(R.id.action_listNoteFragment_to_addNoteFragment)
+        }
 
         return view
     }
