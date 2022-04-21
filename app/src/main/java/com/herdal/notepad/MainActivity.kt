93aa55b104034edae7d086e36420e72f902e0615
herdal06 <h.erdal721@gmail.com>
2022-04-21 06:56:27 +0300

    Setup action bar

--- app/src/main/java/com/herdal/notepad/MainActivity.kt
+++ app/src/main/java/com/herdal/notepad/MainActivity.kt
@@ -2,10 +2,16 @@ package com.herdal.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
+
     }
+
 }
